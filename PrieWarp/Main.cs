ba9670012ddc12ae5b3142b2679b4f65c4da530f
Sean Dempsey <dempsey.sean@outlook.com>
2023-05-29 16:43:29 -0700

    Initial setup

---
+++ PrieWarp/Main.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+
+namespace PrieWarp
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("com.damocles.blasphemous.modding-api", "1.3.4")]
+    public class Main : BaseUnityPlugin
+    {
+        public static PrieWarp PrieWarp;
+
+        private void Start()
+        {
+            PrieWarp = new(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION);
+        }
+    }
+}
---
+++ PrieWarp/PrieWarp.cs
@@ -0,0 +1,9 @@
+﻿using ModdingAPI;
+
+namespace PrieWarp
+{
+    public class PrieWarp : Mod
+    {
+        public PrieWarp(string modId, string modName, string modVersion) : base(modId, modName, modVersion) { }
+    }
+}
